5e9630d1a2052937d7bac34a840b0b2c3c4876ba
Adithravindran0fficial <adithravindranofficil@gmail.com>
2024-08-03 19:53:57 +0530

    added prodct catergory section on navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,8 +4,14 @@ import { FaShoppingCart } from 'react-icons/fa';
 
 function Navbar() {
   const [toggle,settoggle] =  useState(false)
+  const [toggle_cat,setToggle_cat] = useState(false)
+
   const handleToggle = ()=>{
     settoggle(!toggle)
+  }
+
+  const handle_cat = ()=>{
+    setToggle_cat(!toggle_cat)
   }
   return (
     <nav className='container relative mx-auto  p-6 '>
@@ -16,23 +22,35 @@ function Navbar() {
         <img src='https://great-white.in/int/images/greatwhiteelectricals-logo.png ' className='h-10'/>
         </div>
         {/* menu items */}
-        <ul className='hidden items-center  space-x-14 font-medium md:flex'>
-          <li>home</li>
-          <li>products</li>
-          <li>Brands</li>
+        <ul className='hidden items-center  space-x-14 font-medium lg:flex'>
+          <li className='text-gray-900 text-xl font-bold tracking-wide hover:text-gray-700'>home</li>
+          <li className='text-gray-400 text-lg font-medium hover:text-gray-900'onClick={handle_cat}>products</li>
+          <li className='text-gray-400 text-lg font-medium hover:text-gray-900'>Brands</li>
         </ul>
-        <ul className='hidden items-center  space-x-10 font-medium md:flex'>
-          <li><FaShoppingCart/></li>
-          <li>home</li>
+        <ul className='hidden items-center  space-x-10 font-medium lg:flex'>
+          <li className='text-gray-400 text-lg font-medium hover:text-gray-900'><FaShoppingCart/></li>
+          <li className='text-gray-400 text-lg font-medium hover:text-gray-900'>Login</li>
           
         </ul>
 
-        <div className='md:hidden'>
+        <div className='lg:hidden'>
           <FaBars onClick={handleToggle} />
         </div>
 
       </div>
-      {toggle&&<div className='container  mx-auto bg-transparent md:hidden'> 
+      {toggle_cat&&<div className='hidden container bg-slate-200 lg:block'>
+        <ul className='flex justify-around border space-x-10 py-20'>
+          <li onClick={handle_cat} >Wiring and Cables</li>
+          <li  onClick={handle_cat}>Lighting </li>
+          <li  onClick={handle_cat}>Switches and Sockets</li>
+          <li  onClick={handle_cat}>Home Automation</li>
+          <li  onClick={handle_cat}>Electrical Appliances</li>
+          <li onClick={handle_cat}>Solar and Renewable Energy</li>
+        </ul>
+        
+
+      </div>}
+      {toggle&&<div className='container  mx-auto bg-transparent lg:hidden'> 
         <ul className='flex flex-col items-center '>
           <li className='py-2 font-medium' onClick={handleToggle}>home</li>
           <li className='py-2 font-medium' onClick={handleToggle}>about</li>
